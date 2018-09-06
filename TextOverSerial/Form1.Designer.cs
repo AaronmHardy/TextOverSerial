e61ee928a75ceb031299651f2b77ae00762af029
ScramblZ <aaronmichaelhardy@gmail.com>
2018-09-05 20:06:08 -0700

    Version 1.0
    Added
    - Functionality for two rows of text up to 19 characters each
    - Date and time display
    - Optional away message for windows PC's
    - Ability to hide application in system tray

---
+++ TextOverSerial/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace TextOverSerial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(47, 4);
+            this.textBox1.MaxLength = 19;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(47, 36);
+            this.textBox2.MaxLength = 19;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(12, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(125, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hide";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.AllowDrop = true;
+            this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.numericUpDown1.Location = new System.Drawing.Point(72, 64);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(34, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(37, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sent!";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "ASCII Sender is running in the background";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ASCII Sender";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button4.BackgroundImage = global::TextOverSerial.Properties.Resources.calendar;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(12, 36);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(26, 23);
+            this.button4.TabIndex = 9;
+            this.toolTip1.SetToolTip(this.button4, "Click to display current date and time.");
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button3.BackgroundImage = global::TextOverSerial.Properties.Resources.calendar;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(12, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 23);
+            this.button3.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.button3, "Click to display current date and time.");
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(26, 92);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(47, 89);
+            this.textBox3.MaxLength = 19;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(160, 21);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Away Time (minutes)";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.AllowDrop = true;
+            this.numericUpDown2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.numericUpDown2.Location = new System.Drawing.Point(167, 116);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown2.TabIndex = 13;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Baud";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600"});
+            this.comboBox1.Location = new System.Drawing.Point(151, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(56, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.Text = "9600";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 205);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "ASCII Sender";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TextOverSerial/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+
+namespace TextOverSerial
+{
+    public partial class Form1 : Form
+    {
+        // Get idle time from .NET
+        // PInvoke GetLastInputInfo
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        // Create a struct to solve a problem writing to data type LASTINPUTINFO
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+        // Method to call LASTINPUTINFO
+        public static uint GetIdleTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+            return ((uint)Environment.TickCount - lastInPut.dwTime);
+        }
+
+        //Create timer to run UpdateText method
+        private System.Windows.Forms.Timer timer1;
+        public void InitTimer()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(Timer1_Tick); // Event called when timer reaches 0
+            timer1.Interval = 15000; // Sets interval in ms, 1000ms = 1 second
+            timer1.Start(); // Start the timer
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateText(); // Runs UpdateText when timer reaches 0
+        }
+
+		// Initialize variables on startup
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Hide();
+            checkBox1.Checked = false;
+            textBox3.Text = " Away message here";
+            textBox3.Enabled = false;
+            numericUpDown2.Enabled = false;
+        }
+
+        // Used when updating away message or date & time
+        private void UpdateText()
+        {
+            if (DateTimeBox1 == true)
+            {
+                textBox1.Text = DateTime.Now.ToString("ddd'/'dd'/'MMM hh':'mmtt"); // Formatted "Tue/04/Sept 09:35AM"
+            }
+            if (DateTimeBox2 == true)
+            {
+                textBox2.Text = DateTime.Now.ToString("ddd'/'dd'/'MMM hh':'mmtt");
+            }
+            // Away message
+            if (checkBox1.Checked == true && GetIdleTime() > numericUpDown2.Value * 60000)
+            {
+                SendData(textBox1.Text, textBox3.Text); // Writes away message
+            }
+            else
+            {
+                SendData(textBox1.Text, textBox2.Text); // Clears away message
+            }
+        }
+
+        private void SendData(string firstLine, string secondLine)
+        {
+            // Instantiate serial port and open for communication
+            SerialPort port = new SerialPort("COM" + numericUpDown1.Value.ToString(), int.Parse(comboBox1.SelectedItem.ToString()), Parity.None, 8, StopBits.One);
+
+            try // Open the port
+            {
+                port.Open();
+            }
+            catch // Throw message to user if port is unable to open, then return to exit the method
+            {
+                MessageBox.Show("Cannot open serial port "+ numericUpDown1.Value.ToString());
+                return;
+            }
+
+            // Write a set of bytes to clear existing data and return to line 0 char 0
+            port.Write(new byte[] { 31, 13 }, 0, 2);
+
+            // Write first line, return, new line, write second line
+            port.Write(firstLine);
+            port.Write("\r\n");
+            port.Write(secondLine);
+
+            // Housekeeping
+            port.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendData(textBox1.Text, textBox2.Text);
+
+            // If datetime or away message is set
+            if (DateTimeBox1 == true || DateTimeBox2 == true || checkBox1.Checked == true)
+            {
+                InitTimer();
+            }
+
+            // Let the user know text was sent
+            label2.Show();
+        }
+        // Button to hide the window
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(800);
+        }
+        // Button to unhide the window
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+        // Minimize to system tray
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+        // Toggle for date & time buttons
+        bool DateTimeBox1 = false; // Boolean value used to update text
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (DateTimeBox1 == true) // If on toggle off
+            {
+                DateTimeBox1 = false; // do not update text
+                textBox1.Text = "";
+                textBox1.Enabled = true;
+            }
+            else // If off toggle on
+            {
+                DateTimeBox1 = true; // update text
+                textBox1.Text = DateTime.Now.ToString("ddd'/'dd'/'MMM hh':'mmtt");
+                textBox1.Enabled = false;
+            }
+        }
+
+        bool DateTimeBox2 = false; // Boolean value used to update text
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (DateTimeBox2 == true) // If on toggle off
+            {
+                DateTimeBox2 = false; // do not update text
+                textBox2.Text = "";
+                textBox2.Enabled = true;
+            }
+            else // If off toggle on
+            {
+                DateTimeBox2 = true; // update text
+                textBox2.Text = DateTime.Now.ToString("ddd'/'dd'/'MMM hh':'mmtt");
+                textBox2.Enabled = false;
+            }
+        }
+        // Check box to enable / disable away message
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                textBox3.Text = " Away message here";
+                textBox3.Enabled = false;
+                label3.Enabled = false;
+                numericUpDown2.Enabled = false;
+            }
+            // Disable textbox / disable away message
+            else
+            {
+                textBox3.Text = "";
+                textBox3.Enabled = true;
+                label3.Enabled = true;
+                numericUpDown2.Enabled = true;
+            }
+        }
+    }
+}
